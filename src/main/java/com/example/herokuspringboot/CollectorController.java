8df43264bc35e4856cfbd0b433e35659c189c36e
Artur Kovalchuk <arkovalchuk@playtika.com>
2022-04-02 20:46:40 +0300

    Add @Async

--- src/main/java/com/example/herokuspringboot/CollectorController.java
+++ src/main/java/com/example/herokuspringboot/CollectorController.java
@@ -1,26 +1,23 @@
 package com.example.herokuspringboot;
 
 import javax.servlet.http.HttpServletRequest;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
 @RestController
 public class CollectorController {
 
-    private final WebhookBot webhookBot;
+    private final Sender sender;
 
-    public CollectorController(WebhookBot webhookBot) {
-        this.webhookBot = webhookBot;
+    public CollectorController(Sender sender) {
+        this.sender = sender;
     }
 
     @GetMapping("/c")
     void ping(@RequestParam("t") String text,
               HttpServletRequest httpServletRequest) {
-        log.debug("{}", text);
-        webhookBot.sendMessage(text + " " + httpServletRequest.getRemoteAddr());
+        sender.send(text + " " + httpServletRequest.getRemoteAddr());
     }
 
 }
---
+++ src/main/java/com/example/herokuspringboot/Sender.java
@@ -0,0 +1,22 @@
+package com.example.herokuspringboot;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class Sender {
+
+    private final WebhookBot webhookBot;
+
+    public Sender(WebhookBot webhookBot) {
+        this.webhookBot = webhookBot;
+    }
+
+    @Async
+    public void send(String text) {
+        webhookBot.sendMessage(text);
+    }
+
+}
